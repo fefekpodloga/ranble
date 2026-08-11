2ae5ff647ffd95db024e9d15cf9dc023dd673442
fefekpodloga <filip.dederko@proton.me>
2026-08-11 22:49:58 +0200

    saving data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Widget from "./Widget/Widget";
 
 export default function App() {
-    const [list, setList] = useState([]);
-    const [testament, setTestament] = useState("new");
+    const [list, setList] = useState(() => {
+        const savedList = localStorage.getItem("list");
+        // get saved data. if data exists return JSON.parse(savedList) if not return []
+        // JSON.parse(savedList) converts datra saved in the JSON format to an array
+        return savedList ? JSON.parse(savedList) : [];
+    });
+    const [testament, setTestament] = useState(() => {
+        // return saved testament value or default value "new"
+        return localStorage.getItem("testament") || "new";
+    });
+
+    // save both list and testament after every change made in the app (like adding a new widget)
+    useEffect(() => {
+        // it saves the value of 'list' under the key "list". because it's not an string it need JSON.stringify()
+        localStorage.setItem("list", JSON.stringify(list));
+        // [list] is an dependency array. it saves the value only if it's changed
+    }, [list]);
     
+    useEffect(() => {
+        // here is no need for JSON.stringify() because 'testament' is a string
+        localStorage.setItem("testament", JSON.stringify(testament));
+    }, [testament]);
+
     const add = () => setList([...list, 1]);
     const changeTestament = (e) => setTestament(e.target.value);
-    
+
     return (
         <>
             <button className="add" onClick={add}>Add</button>
